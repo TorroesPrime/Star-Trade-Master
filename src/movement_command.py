dab3a3334a724df5e748e015573c6c1c09c26173
TorroesPrime <michael.cividanes2014@gmail.com>
2020-08-13 23:20:28 -0400

    Update movement_command.py

--- src/movement_command.py
+++ src/movement_command.py
@@ -1,11 +1,16 @@
-from command import Command
+from interpreter import game_state_instance
 
-class MovementCommand(Command):
+class MovementCommand():
     commandString = ""
     MOVEMENT_COMMANDS = "nsweud"
 
-    def __init__(self,s):
-        self.dir = s
+    def __init__(self,direction):
+        self.dir = direction
 
     def execute(self):
-        print("You moved")
+        currentRoom = game_state_instance.get_adventurers_current_room()
+        nextRoom = currentRoom.leave_by(dir)
+        if nextRoom != None:
+            game_state_instance.set_adventurers_current_room(nextRoom)
+        else:
+            print(f"Sorry, you can't go {self.dir} from {currentRoom.name}.")
